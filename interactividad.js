5b7ec400dd416a4bd256df3e657b8342db69364a
Andrés Fernández <lafvg12@hotmail.com>
2020-01-20 21:27:15 -0500

    primer commit

---
+++ interactividad.js
@@ -0,0 +1,12 @@
+ var andres = {
+     age : 28,
+     name : "Andres",
+     hobby : "read"
+ }
+
+ function nombreMayuscula() {
+     name = name.toUpperCase()
+     console.log(name)
+ }
+
+ nombreMayuscula()
